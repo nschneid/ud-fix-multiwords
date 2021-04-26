0f29644d909ff25e67f67cc550bfdc0975340a4b
Ethan Chi <5086557+ethanachi@users.noreply.github.com>
2021-04-26 13:48:52 -0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+
+
+class colors:
+    """Defines shell escapes for colored text output."""
+
+    OKPURPLE = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLACKBACKGROUND='\033[40m\033[37m'
+
